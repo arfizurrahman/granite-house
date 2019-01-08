9e383df754f5df7316d1a2de3fbefa4f42325963
arfizurrahman <arfizrahman0@gmail.com>
2019-01-08 16:15:48 +0600

    Modified folder structure

--- GraniteHouse/Startup.cs
+++ GraniteHouse/Startup.cs
@@ -67,7 +67,7 @@ namespace GraniteHouse
             {
                 routes.MapRoute(
                     name: "areas",
-                    template: "{area:Customer}/{controller=Home}/{action=Index}/{id?}"
+                    template: "{area=Customer}/{controller=Home}/{action=Index}/{id?}"
                 );
             });
 
