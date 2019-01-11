3ddabd21f5d7bcfb62879109d3ded5a28b0d12b4
arfizurrahman <arfizrahman0@gmail.com>
2019-01-11 11:57:39 +0600

    Configured hosting environment and implemented static details class

--- GraniteHouse/Controllers/ProductsController.cs
+++ GraniteHouse/Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using GraniteHouse.Areas.Admin.Controllers;
 using GraniteHouse.Data;
 using GraniteHouse.Models;
 using GraniteHouse.Models.ViewModels;
+using Microsoft.AspNetCore.Hosting.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,13 +16,15 @@ namespace GraniteHouse.Controllers
     public class ProductsController : Controller
     {
         private readonly ApplicationDbContext _db;
+        private readonly HostingEnvironment _hostingEnvironment;
 
         [BindProperty]
         public ProductViewModel ProductVM { get; set; }
 
-        public ProductsController(ApplicationDbContext db)
+        public ProductsController(ApplicationDbContext db, HostingEnvironment hostingEnvironment)
         {
             _db = db;
+            _hostingEnvironment = hostingEnvironment;
             ProductVM = new ProductViewModel()
             {
                 ProductTypes = _db.ProductTypes.ToList(),
@@ -34,6 +37,30 @@ namespace GraniteHouse.Controllers
         {
             var products = _db.Products.Include(m=>m.ProductType).Include(m=>m.SpecialTag);
             return View(await products.ToListAsync());
+        }
+
+        //GET: Products create
+        public IActionResult Create()
+        {
+            return View(ProductVM);
+        }
+
+        //POST : Products Create
+        [HttpPost, ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreatePOST()
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(ProductVM);
+            }
+
+            _db.Products.Add(ProductVM.Product);
+            await _db.SaveChangesAsync();
+
+            //Save image
+            string webRootPath = _hostingEnvironment.WebRootPath;
+
         }
     }
 }
---
+++ GraniteHouse/Utilities/StaticDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Utilities
+{
+    public class StaticDetails
+    {
+        public const string DefaultProductImage = "default_product.jpg";
+        public const string ImageFolder = @"images\product-image";
+    }
+}
