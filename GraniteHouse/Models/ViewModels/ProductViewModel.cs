c8816e84a58c32face32a21b70c2b3d0cdaa41b9
arfizurrahman <arfizrahman0@gmail.com>
2019-01-11 09:48:56 +0600

    Added product view model

---
+++ GraniteHouse/Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Models.ViewModels
+{
+    public class ProductViewModel
+    {
+        public Product Product { get; set; }
+        public IEnumerable<ProductType> ProductTypes { get; set; }
+        public IEnumerable<SpecialTag> SpecialTags { get; set; }
+    }
+}
