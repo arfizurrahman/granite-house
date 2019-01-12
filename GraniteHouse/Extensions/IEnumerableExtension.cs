1ebafc66dec363dca306587754d658bceefcbe1c
arfizurrahman <arfizrahman0@gmail.com>
2019-01-12 19:35:58 +0600

    Completed adding view elements for creating product

--- GraniteHouse/Extensions/IEnumerableExtension.cs
+++ GraniteHouse/Extensions/IEnumerableExtension.cs
@@ -8,14 +8,14 @@ namespace GraniteHouse.Extensions
 {
     public static class IEnumerableExtension
     {
-        public static IEnumerable<SelectListItem> ToSelectListItem<T>(this IEnumerable<T> items, string selectedValue)
+        public static IEnumerable<SelectListItem> ToSelectListItem<T>(this IEnumerable<T> items, int selectedValue)
         {
             return from item in items
                 select new SelectListItem
                 {
                     Text = item.GetPropertyValue("Name"),
                     Value = item.GetPropertyValue("Id"),
-                    Selected = item.GetPropertyValue("Id").Equals(selectedValue)
+                    Selected = item.GetPropertyValue("Id").Equals(selectedValue.ToString())
                 };
         }
     }
