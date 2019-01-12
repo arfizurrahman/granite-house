1f65c01894d64532ce3e11f4060bedc72c902dac
arfizurrahman <arfizrahman0@gmail.com>
2019-01-12 12:37:34 +0600

    Implemented image upload functionalities

--- GraniteHouse/Controllers/ProductsController.cs
+++ GraniteHouse/Controllers/ProductsController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using GraniteHouse.Areas.Admin.Controllers;
 using GraniteHouse.Data;
 using GraniteHouse.Models;
 using GraniteHouse.Models.ViewModels;
+using GraniteHouse.Utilities;
 using Microsoft.AspNetCore.Hosting.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -58,8 +60,47 @@ namespace GraniteHouse.Controllers
             _db.Products.Add(ProductVM.Product);
             await _db.SaveChangesAsync();
 
+            //Retrieve the newly saved product from database for saving image
+            var productFromDb = _db.Products.Find(ProductVM.Product.Id);
+
             //Save image
             string webRootPath = _hostingEnvironment.WebRootPath;
+
+            //retrieve all files
+            var files = HttpContext.Request.Form.Files;
+
+            //check if user has uploaded a file
+            if (files.Count !=0)
+            {
+                //Image has been uploaded
+                var uploads = Path.Combine(webRootPath, StaticDetails.ImageFolder);
+                //Get the file extension
+                var extension = Path.GetExtension(files[0].FileName);
+
+                //Copy file from upload to server
+                using (var filestream = new FileStream(Path.Combine(uploads, ProductVM.Product.Id + extension), FileMode.Create))
+                {
+                    //Save to file to server
+                    files[0].CopyTo(filestream);
+                }
+
+                //Update the image column with the image path
+                productFromDb.Image = @"\" + StaticDetails.ImageFolder + @"\" + ProductVM.Product.Id + extension;
+            }
+            else
+            {
+                //iF user doesn't select a file, add the default image path
+                var uploads = Path.Combine(webRootPath,
+                    StaticDetails.ImageFolder + @"\" + StaticDetails.DefaultProductImage);
+                System.IO.File.Copy(uploads, webRootPath + @"\" + StaticDetails.ImageFolder + @"\" + ProductVM.Product.Id + ".png");
+                productFromDb.Image = @"\" + StaticDetails.ImageFolder + @"\" + ProductVM.Product.Id + ".png";
+
+            }
+
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+
 
         }
     }
