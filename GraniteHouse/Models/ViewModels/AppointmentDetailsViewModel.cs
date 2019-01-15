1e6e872ebf7e40c01a8c010a6a07ef321a347186
arfizurrahman <arfizrahman0@gmail.com>
2019-01-15 17:57:47 +0600

    Added appointment view model

---
+++ GraniteHouse/Models/ViewModels/AppointmentDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Models.ViewModels
+{
+    public class AppointmentDetailsViewModel
+    {
+        public Appointment Appointment { get; set; }
+        public List<Product> Products { get; set; }
+        public List<ApplicationUser> SalesPersons { get; set; }
+    }
+}
