203c70da4f9f19ec02b4bb3d75a734da67291567
arfizurrahman <arfizrahman0@gmail.com>
2019-01-17 14:36:27 +0600

    Added new property to AppointmentViewModel

--- GraniteHouse/Models/ViewModels/AppointmentViewModel.cs
+++ GraniteHouse/Models/ViewModels/AppointmentViewModel.cs
@@ -8,5 +8,6 @@ namespace GraniteHouse.Models.ViewModels
     public class AppointmentViewModel
     {
         public List<Appointment> Appointments { get; set; }
+        public PagingInfo PagingInfo { get; set; }
     }
 }
