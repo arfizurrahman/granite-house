f154954d886059afa03f8a7095c142fa77d9444c
arfizurrahman <arfizrahman0@gmail.com>
2019-01-17 20:05:46 +0600

    Added view component for user name

---
+++ GraniteHouse/ViewComponents/UserNameViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using GraniteHouse.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraniteHouse.ViewComponents
+{
+    public class UserNameViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserNameViewComponent(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdemtity = (ClaimsIdentity) this.User.Identity;
+            var claims = claimsIdemtity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var userFromDb = await _db.ApplicationUsers.Where(u => u.Id == claims.Value).FirstOrDefaultAsync();
+
+            return View(userFromDb);
+        }
+    }
+}
