885eae7f8408364541f30c7c42ba59306ccd3ee7
arfizurrahman <arfizrahman0@gmail.com>
2019-01-17 14:27:21 +0600

    Added custom tag helper class for pagination

--- GraniteHouse/TagHelpers/PageLinkTagHelper.cs
+++ GraniteHouse/TagHelpers/PageLinkTagHelper.cs
@@ -2,7 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GraniteHouse.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace GraniteHouse.TagHelpers
@@ -15,6 +20,41 @@ namespace GraniteHouse.TagHelpers
         public PageLinkTagHelper(IUrlHelperFactory urlHelperFactory)
         {
             _urlHelperFactory = urlHelperFactory;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public PagingInfo PageModel { get; set; }
+        public string PageAction { get; set; }
+        public bool PageClassesEnabled { get; set; }
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = _urlHelperFactory.GetUrlHelper(ViewContext);
+            TagBuilder result = new TagBuilder("div");
+
+            for (int i = 1; i <= PageModel.totalPage; i++)
+            {
+                TagBuilder ancorTag = new TagBuilder("a");
+                string url = PageModel.UrlParam.Replace(":", i.ToString());
+                ancorTag.Attributes["href"] = url;
+
+                if (PageClassesEnabled)
+                {
+                    ancorTag.AddCssClass(PageClass);
+                    ancorTag.AddCssClass(i==PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                }
+
+                ancorTag.InnerHtml.Append(i.ToString());
+                result.InnerHtml.AppendHtml(ancorTag);
+            }
+
+            output.Content.AppendHtml(result.InnerHtml);
         }
     }
 }
