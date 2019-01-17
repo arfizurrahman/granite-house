63146b05bc85838056c516ab9774bfddafa48846
arfizurrahman <arfizrahman0@gmail.com>
2019-01-17 11:56:46 +0600

    Created new PageLinkTagHelper class for pagination

---
+++ GraniteHouse/TagHelpers/PageLinkTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace GraniteHouse.TagHelpers
+{
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PageLinkTagHelper : TagHelper
+    {
+        private IUrlHelperFactory _urlHelperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory urlHelperFactory)
+        {
+            _urlHelperFactory = urlHelperFactory;
+        }
+    }
+}
