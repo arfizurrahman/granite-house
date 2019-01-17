90cd1ca3d328032f86cd2eaee53f9d8fcf3b0327
arfizurrahman <arfizrahman0@gmail.com>
2019-01-17 11:10:32 +0600

    Added PagingInfo model for custom tag helper

---
+++ GraniteHouse/Models/PagingInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Models
+{
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        public int totalPage => (int) Math.Ceiling((decimal) TotalItems / ItemsPerPage);
+
+        // This will be used to build URL
+        public string UrlParam { get; set; }
+    }
+}
